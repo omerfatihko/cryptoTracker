9d3373d743218995f7cec2d737da660b09831f1d
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-03-14 02:35:30 +0300

    better header

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
     import '@fontsource-variable/orbitron/wght.css';
 </script>
-<div class="header">
-    <a href="/"><img src="src/public/Ko-RyptoLogo.jpg" alt="site logo" height="48"></a>
-    <h1>KO-RYPTO</h1>
-</div>
+
+<header>
+    <div class="header">
+        <a href="/"><img src="src/public/Ko-RyptoLogo.jpg" alt="site logo" height="48"></a>
+        <h1>KORYPTO</h1>
+    </div>
+</header>
 
 <style>
-    * {
-		font-family: 'Orbitron Variable', sans-serif;
-        font-weight: 900;
-        font-size: 3rem;
-	}
+    .header {
+        display: flex;
+        flex-wrap: wrap;
+    }
     h1 {
         display: inline-block;
+        font-family: 'Orbitron Variable', sans-serif;
+        font-weight: 900;
+        font-size: 2.25rem;
+        padding: 0;
+        margin-left: 5px;
+        margin-right: 5px;
+        margin-top: 0;
+        margin-bottom: 0;
     }
-    .header {
-
+    img {
+        margin: 0;
     }
 </style>

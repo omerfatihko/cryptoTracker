8326a67d03cdd8dcc16f689b1610ff81725f24df
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-03-13 23:38:15 +0300

    added some fonts and started a header

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import '@fontsource-variable/orbitron/wght.css';
+</script>
+<div class="header">
+    <a href="/"><img src="src/public/Ko-RyptoLogo.jpg" alt="site logo" height="48"></a>
+    <h1>KO-RYPTO</h1>
+</div>
+
+<style>
+    * {
+		font-family: 'Orbitron Variable', sans-serif;
+        font-weight: 900;
+        font-size: 3rem;
+	}
+    h1 {
+        display: inline-block;
+    }
+    .header {
+
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script>
 	import '../app.pcss';
+/* 	import '@fontsource/geist-sans';
+	import '@fontsource-variable/orbitron'; */
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <!--header and Navbar that includes language options and dark mode. Also responsive -->
-
+<Navbar/>
 <slot />
+<Footer/>
 
 <!-- Footer with abouts page link -->
+
+<style>
+</style>

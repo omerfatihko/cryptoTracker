781272d0f561943f7fe2685cf2f248fda4717f2b
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-03-13 15:07:01 +0300

    added logo, tailwind and shadcn svelte ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.pcss';
+</script>
+
+<!--header and Navbar that includes language options and dark mode. Also responsive -->
+
+<slot />
+
+<!-- Footer with abouts page link -->

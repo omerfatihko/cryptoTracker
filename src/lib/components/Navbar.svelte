d80d5ac029848a7d084a58c067b510a7358ca73c
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-03-14 14:48:44 +0300

    fixed broken tailwind, mobile first and responsive header and navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,31 +1,23 @@
 <script lang="ts">
     import '@fontsource-variable/orbitron/wght.css';
+    import Button from './ui/button/button.svelte';
 </script>
 
-<header>
-    <div class="header">
-        <a href="/"><img src="src/public/Ko-RyptoLogo.jpg" alt="site logo" height="48"></a>
-        <h1>KORYPTO</h1>
+<header class= "flex flex-wrap justify-center sm:justify-between">
+    <div class=" flex flex-nowrap flex-none">
+        <a href="/"><img class="h-10 m-0" src="src/public/Ko-RyptoLogo.jpg" alt="site logo"></a>
+        <h1 class="my-0 mx-1.5 p-0 font-black text-4xl inline-block">KORYPTO</h1>
     </div>
+    <nav class="grow sm:grow-0">
+        <div class="flex justify-center sm:justify-end">
+            <Button variant="ghost">Home</Button>
+            <Button variant="ghost">About</Button>
+        </div>
+    </nav>
 </header>
 
 <style>
-    .header {
-        display: flex;
-        flex-wrap: wrap;
-    }
     h1 {
-        display: inline-block;
         font-family: 'Orbitron Variable', sans-serif;
-        font-weight: 900;
-        font-size: 2.25rem;
-        padding: 0;
-        margin-left: 5px;
-        margin-right: 5px;
-        margin-top: 0;
-        margin-bottom: 0;
-    }
-    img {
-        margin: 0;
     }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -55,8 +55,7 @@ const config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans],
-				orbitron: ["Orbitron", "sans-serif"]
+				sans: [...fontFamily.sans]
 			}
 		}
 	},

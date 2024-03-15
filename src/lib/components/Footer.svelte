321c5c4a83f516882cffdddb9e729612d7cd6824
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-03-15 18:07:09 +0300

    added a footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Separator } from "$lib/components/ui/separator";
+</script>
+
+<footer class="absolute bottom-0 w-full text-center">
+    <Separator class="my-2"/>
+    <div>
+        <p >Developed by DevbyKo</p>
+    </div>
+</footer>

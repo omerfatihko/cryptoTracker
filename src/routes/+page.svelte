8832318dc4921662ae500b6850fc382ab743b307
Ömer Fatih Konar <omerfatihk@gmail.com>
2024-03-18 18:10:09 +0300

    a desirable date picker added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
     import { coinStore } from "$lib/stores/stores";
     import Checkbox from '$lib/components/Checkbox.svelte';
     import CoinCard from "$lib/components/CoinCard.svelte";
-
+    import DatePicker from "$lib/components/DatePicker.svelte";
+	import type { DateValue } from "@internationalized/date";
+    let date: DateValue | undefined;
 </script>
 
 <Checkbox/>
+<DatePicker bind:value={date}></DatePicker>
+<p>{date}</p>
 
 {#each $coinStore as coin}
     {#if coin.checked}
